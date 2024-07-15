64541babbd0adc04e581a40edb190764a1591fd5
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-07-15 14:04:45 -0400

    basic calendar component done

---
+++ src/Calendar.js
@@ -0,0 +1,50 @@
+import CalendarTile from "./CalendarTile";
+import './style/CalendarStyle.css';
+
+export default function Calendar() {
+    return(
+        <div>
+            <div>Welcome to the Calendar. Here, you can manage all of your birthdays
+                in a convenient calendar view.
+            </div>
+            <div>
+                <div style={{float:'left', marginLeft: '15%',display: "block"}}>
+                    <h1>Month</h1>
+                </div>
+                <br></br><br></br>
+                <div class="row1">
+                    <CalendarTile date={1}></CalendarTile>
+                    <CalendarTile date={2}></CalendarTile>
+                    <CalendarTile date={3}></CalendarTile>
+                    <CalendarTile date={4}></CalendarTile>
+                    <CalendarTile date={5}></CalendarTile>
+                    <CalendarTile date={6}></CalendarTile>
+                    <CalendarTile date={7}></CalendarTile>
+                    <CalendarTile date={8}></CalendarTile>
+                    <CalendarTile date={9}></CalendarTile>
+                    <CalendarTile date={10}></CalendarTile>
+                    <CalendarTile date={11}></CalendarTile>
+                    <CalendarTile date={12}></CalendarTile>
+                    <CalendarTile date={13}></CalendarTile>
+                    <CalendarTile date={14}></CalendarTile>
+                    <CalendarTile date={15}></CalendarTile>
+                    <CalendarTile date={16}></CalendarTile>
+                    <CalendarTile date={17}></CalendarTile>
+                    <CalendarTile date={18}></CalendarTile>
+                    <CalendarTile date={19}></CalendarTile>
+                    <CalendarTile date={20}></CalendarTile>
+                    <CalendarTile date={21}></CalendarTile>
+                    <CalendarTile date={22}></CalendarTile>
+                    <CalendarTile date={23}></CalendarTile>
+                    <CalendarTile date={24}></CalendarTile>
+                    <CalendarTile date={25}></CalendarTile>
+                    <CalendarTile date={26}></CalendarTile>
+                    <CalendarTile date={27}></CalendarTile>
+                    <CalendarTile date={28}></CalendarTile>
+                    <CalendarTile date={29}></CalendarTile>
+                    <CalendarTile date={30}></CalendarTile>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/HomeScreen.js
@@ -0,0 +1,7 @@
+export default function HomeScreen() {
+    return(
+        <div>Hello! Welcome to Birthday Manager.
+            <br></br>Click <a href="/calendar-view">here</a> to go to Calendar view.
+        </div>
+    )
+}
