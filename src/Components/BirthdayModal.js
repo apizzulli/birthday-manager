311e363109e3abdb993d93293849157d38af4c15
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-07-16 20:26:13 -0400

    working with close now toogit add .! :D

--- src/Components/BirthdayModal.js
+++ src/Components/BirthdayModal.js
@@ -4,11 +4,11 @@ import Button from '@mui/joy/Button';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useContext, useState } from 'react';
 
-export default function BirthdayModal({props}) {
+export default function BirthdayModal({toggle}) {
     const [birthdayCreated, setBirthdayCreated] = useState(false);
     return(   
         <div class="modal-wrapper">
-            <div style={{width:'100%', overflow:'auto'}}><CancelIcon onClick={()=> props.toggle(false)} style={{float:'right', marginRight:'5px',marginTop:'5px'}}></CancelIcon> </div>
+            <div style={{width:'100%', overflow:'auto'}}><CancelIcon onClick={()=> toggle(false)} style={{float:'right', marginRight:'5px',marginTop:'5px'}}></CancelIcon> </div>
             <div class="birthday-modal">
                 <h1>Fill out information for the birthday</h1>
                 <div class="input-wrapper">
--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -17,36 +17,36 @@ export default function Calendar() {
                 </div>
                 <br></br><br></br>
                 <div class="row1">
-                    <CalendarTile  modal={toggleModal} date={1}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={2}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={3}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={4}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={5}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={6}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={7}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={8}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={9}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={10}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={11}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={12}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={13}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={14}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={15}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={16}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={17}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={18}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={19}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={20}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={21}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={22}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={23}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={24}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={25}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={26}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={27}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={28}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={29}></CalendarTile>
-                    <CalendarTile  modal={toggleModal} date={30}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={1}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={2}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={3}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={4}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={5}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={6}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={7}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={8}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={9}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={10}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={11}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={12}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={13}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={14}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={15}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={16}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={17}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={18}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={19}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={20}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={21}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={22}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={23}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={24}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={25}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={26}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={27}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={28}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={29}></CalendarTile>
+                    <CalendarTile modal={toggleModal} date={30}></CalendarTile>
                     <Modal open={modal} close={modal}><BirthdayModal toggle={toggleModal}></BirthdayModal></Modal>
                 </div>
             </div> 
