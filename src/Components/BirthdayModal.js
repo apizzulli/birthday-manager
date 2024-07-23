0b35bc5b11cb0c10de188592f3e66f1d7992f868
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-07-23 16:02:22 -0400

    things are happening!

--- src/Components/BirthdayModal.js
+++ src/Components/BirthdayModal.js
@@ -1,4 +1,4 @@
-import '../style/BirthdayModal.css';
+import '../style/birthday_modal_style.css';
 import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
 import CancelIcon from '@mui/icons-material/Cancel';
@@ -20,21 +20,24 @@ export default function BirthdayModal(props) {
     }
 
     return(   
-        <div class="modal-wrapper">
-            <div style={{width:'100%', overflow:'auto'}}><CancelIcon onClick={()=>props.closeModal(false)} style={{float:'right', marginRight:'5px',marginTop:'5px'}}></CancelIcon> </div>
-            <form class="modal-form" style={{visibility: name === null ? 'visible': 'hidden'}}>
-                <h1>Fill out information for the birthday</h1>
-                <div class="input-wrapper" > 
-                    <Input name="name" sx={{width: 300, marginBottom: '3%'}} placeholder="Name" error={displayError} helperText={"A name is required."} required></Input>
-                    <Input sx={{width: 300, marginBottom: '3%'}} placeholder="Note"> </Input>
-                    <Input sx={{width: 300, marginBottom: '3%'}} placeholder="Reminder"> </Input>
-                    <Button onClick={handleSubmit} sx={{width: 100, marginBottom: '3%', display: name === null ? 'block': 'none', marginTop:'6%'}} variant = "plain">Create</Button>
-                    <h3 style={{display: name === null ? 'none': 'block'}}>Birthday Created!</h3>
-                </div>
-            </form>
-            <div style={{display: name === null ? 'none': 'block'}}>
-                <h1>{name}'s Birthday</h1>
+        <div class="new-birthday-wrapper">
+            <div style={{width:'100%'}}><CancelIcon onClick={()=>props.closeModal(false)} style={{float:'right', marginRight:'5px',marginTop:'5px'}}></CancelIcon> </div>
+            <div style={{display: name === null ? 'block': 'none'}}>
+                <form class="modal-form">
+                    <h1>Fill out information for the birthday</h1>
+                    <div class="input-wrapper" > 
+                        <Input name="name" sx={{width: 300, marginBottom: '3%'}} placeholder="Name" error={displayError} helperText={"A name is required."} required></Input>
+                        <Input sx={{width: 300, marginBottom: '3%'}} placeholder="Note"> </Input>
+                        <Input sx={{width: 300, marginBottom: '3%'}} placeholder="Reminder"> </Input>
+                        <Button onClick={handleSubmit} sx={{width: 100, marginBottom: '3%', display: name === null ? 'block': 'none', marginTop:'6%'}} variant = "plain">Create</Button>
+                        <h3 style={{display: name === null ? 'none': 'block'}}>Birthday Created!</h3>
+                    </div>
+                </form>
+            </div>
+            <div class="birthday-info" style={{display: name === null ? 'none': 'flex'}}>
                 <h3>{props.month} {props.date}</h3>
+                <h1>{name}'s Birthday</h1>
+
             </div> 
         </div>
     );
