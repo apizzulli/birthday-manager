16532ea204d6a84487248e44423be6e7062f7776
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-07-22 22:13:14 -0400

    basic menu working

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -3,17 +3,52 @@ import '../style/calendar_style.css';
 import { useState, useContext } from 'react';
 import Modal from '@mui/joy/Modal';
 import BirthdayModal from "./BirthdayModal";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function Calendar() {
+    const [selectedMonth, setSelectedMonth] = useState("January");
     const[modal, toggleModal] = useState(false);
-    const handleClick = () =>{
-        console.log('hi');
+    const [anchorEl, setAnchorEl] = useState(null);
+    let open = Boolean(anchorEl);
+
+    const handleMenuOpen = (event) => {
+      setAnchorEl(event.currentTarget.innerHTML);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const handleMonthSelect = (event) => {
+        setSelectedMonth(event.currentTarget.innerText);
     }
+  
     return(
         <div >
             <div>
-                <div style={{float:'left',marginLeft:'8%',display: "block"}}>
-                    <h1>Month</h1>
+                <div style={{float: 'left',marginLeft:"7%"}}>
+                    <h1>{selectedMonth}</h1>
+                    <ArrowDropDownIcon onClick={handleMenuOpen}></ArrowDropDownIcon>
+                    <Menu anchorEl={anchorEl}
+                        open={open}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}>   
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>February</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>March</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>April</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>May</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>June</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>July</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>August</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>September</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>October</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>November</MenuItem>
+                        <MenuItem onClick={(event)=>handleMonthSelect(event)}>December</MenuItem>
+                    </Menu>
                 </div>
                 <br></br><br></br>
                 <div class="row1">
