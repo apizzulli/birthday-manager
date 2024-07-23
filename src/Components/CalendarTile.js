416bf97826c9e14c66bd65e989394567c152b880
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-07-22 21:18:00 -0400

    FIXEDgit add . CAN NOW CLOSE MODALgit add .git add .! issue is that the onClick was registering for the entire screen as the tile

--- src/Components/CalendarTile.js
+++ src/Components/CalendarTile.js
@@ -1,6 +1,6 @@
 import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 import '../style/calendar_style.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import StarIcon from '@mui/icons-material/Star';
 import Modal from '@mui/joy/Modal';
 import BirthdayModal from './BirthdayModal';
@@ -8,12 +8,24 @@ import BirthdayModal from './BirthdayModal';
 export default function CalendarTile({date}) {
     const [hovered, setHovered] = useState(false);
     const [selected, setSelected] = useState(false);
-    const[modal, toggleModal] = useState(false);
+    const[modalVisible, setModalVisible] = useState(false);
+    let help = false;
+    const handleCloseModal = () => {
+        setModalVisible(false);
+        help = true;
+    }
+
+    // useEffect(()=>{
+    //     if(help)
+    //         setModalVisible(false);
+    //     console.log("modalVisible = "+modalVisible);
+    // });
+
     return(
-        <div class="calendar-tile" onClick={()=> toggleModal(true)} onMouseOver={()=> setHovered(true)} onMouseLeave={()=> setHovered(false)}style={{backgroundColor: hovered ? '#D8F8FE' : 'white'}}>
+        <div class="calendar-tile" onMouseOver={()=> setHovered(true)} onMouseLeave={()=> setHovered(false)}style={{backgroundColor: hovered ? '#D8F8FE' : 'white'}}>
             <StarIcon style={{float:'left',marginRight:'7%',height:'25%', width:'25%', visibility: selected ? 'visible' : 'hidden'}}></StarIcon>
-            <h1>{date}</h1>
-            <Modal open={modal} close={modal}><BirthdayModal toggle={toggleModal}></BirthdayModal></Modal>
+            <h1 onClick={()=> setModalVisible(true)}>{date}</h1>
+            <Modal open={modalVisible}><BirthdayModal closeModal={setModalVisible}></BirthdayModal></Modal>
         </div>
     )
 }
