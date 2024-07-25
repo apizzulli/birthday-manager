7a9cfdd01d9d7a5e3d41f3d920e9f3b79d1ea9bb
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-07-24 21:02:30 -0400

    changes from work

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -3,100 +3,46 @@ import '../style/calendar_style.css';
 import { useState, useContext } from 'react';
 import Modal from '@mui/joy/Modal';
 import BirthdayModal from "./BirthdayModal";
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
-export default function Calendar() {
+export default function Calendar(props) {
     const [thirtyOneDays, setThirtyOneDays] = useState(true);
-    const [selectedMonth, setSelectedMonth] = useState("January");
     const[modal, toggleModal] = useState(false);
-    const [anchorEl, setAnchorEl] = useState(null);
-    let open = Boolean(anchorEl);
-
-    const handleMenuOpen = (event) => {
-      setAnchorEl(event.currentTarget.innerHTML);
-    };
-
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-
-    const handleMonthSelect = (event) => {
-        let month = event.currentTarget.innerText;
-        if( month==="April" || month==="June" || month==="September" || month==="November"){
-            setThirtyOneDays(false);
-        }
-        setSelectedMonth(month);
-        setAnchorEl(null);
-    }
   
     return(
         <div style={{width:'100%'}}>
             <div style={{width:'100%'}}>
-                <div style={{float: 'left',marginLeft:"7%", display:'flex', alignItems:'center', width:'15%', columnGap:'10%'}}>
-                    <h1 style={{width:'50%'}}>{selectedMonth}</h1>
-                    <div style={{width:'50%'}}>
-                        <ArrowDropDownIcon style={{display:anchorEl ? 'none': 'block'}}onClick={handleMenuOpen}></ArrowDropDownIcon>
-                        <ArrowDropUpIcon style={{display:anchorEl ? 'block': 'none'}}onClick={()=>setAnchorEl(null)}></ArrowDropUpIcon>
-                    </div>
-                    <Menu anchorEl={anchorEl}
-                        open={open}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                          }}
-                          transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                          }}>   
-                        <MenuItem selected={selectedMonth==="January"} onClick={(event)=>handleMonthSelect(event)}>January</MenuItem>
-                        <MenuItem selected={selectedMonth==="February"} onClick={(event)=>handleMonthSelect(event)}>February</MenuItem>
-                        <MenuItem selected={selectedMonth==="March"} onClick={(event)=>handleMonthSelect(event)}>March</MenuItem>
-                        <MenuItem selected={selectedMonth==="April"} onClick={(event)=>handleMonthSelect(event)}>April</MenuItem>
-                        <MenuItem selected={selectedMonth==="May"} onClick={(event)=>handleMonthSelect(event)}>May</MenuItem>
-                        <MenuItem selected={selectedMonth==="June"} onClick={(event)=>handleMonthSelect(event)}>June</MenuItem>
-                        <MenuItem selected={selectedMonth==="July"} onClick={(event)=>handleMonthSelect(event)}>July</MenuItem>
-                        <MenuItem selected={selectedMonth==="August"} onClick={(event)=>handleMonthSelect(event)}>August</MenuItem>
-                        <MenuItem selected={selectedMonth==="September"} onClick={(event)=>handleMonthSelect(event)}>September</MenuItem>
-                        <MenuItem selected={selectedMonth==="October"} onClick={(event)=>handleMonthSelect(event)}>October</MenuItem>
-                        <MenuItem selected={selectedMonth==="November"} onClick={(event)=>handleMonthSelect(event)}>November</MenuItem>
-                        <MenuItem selected={selectedMonth==="December"} onClick={(event)=>handleMonthSelect(event)}>December</MenuItem>
-                    </Menu>
-                </div>
                 <br></br><br></br>
                 <div class="row1">
-                    <CalendarTile date={1} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={2} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={3} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={4} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={5} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={6} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={7} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={8} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={9} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={10} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={11} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={12} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={13} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={14} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={15} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={16} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={17} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={18} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={19} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={20} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={21} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={22} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={23} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={24} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={25} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={26} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={28} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={29} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={30} month ={selectedMonth}></CalendarTile>
-                    <CalendarTile date={31} month ={selectedMonth} ></CalendarTile>
+                    <CalendarTile month={props.month} date={1} ></CalendarTile>
+                    <CalendarTile date={2} ></CalendarTile>
+                    <CalendarTile date={3} ></CalendarTile>
+                    <CalendarTile date={4} ></CalendarTile>
+                    <CalendarTile date={5} ></CalendarTile>
+                    <CalendarTile date={6} ></CalendarTile>
+                    <CalendarTile date={7} ></CalendarTile>
+                    <CalendarTile date={8} ></CalendarTile>
+                    <CalendarTile date={9} ></CalendarTile>
+                    <CalendarTile date={10} ></CalendarTile>
+                    <CalendarTile date={11} ></CalendarTile>
+                    <CalendarTile date={12} ></CalendarTile>
+                    <CalendarTile date={13} ></CalendarTile>
+                    <CalendarTile date={14} ></CalendarTile>
+                    <CalendarTile date={15} ></CalendarTile>
+                    <CalendarTile date={16} ></CalendarTile>
+                    <CalendarTile date={17} ></CalendarTile>
+                    <CalendarTile date={18} ></CalendarTile>
+                    <CalendarTile date={19} ></CalendarTile>
+                    <CalendarTile date={20} ></CalendarTile>
+                    <CalendarTile date={21} ></CalendarTile>
+                    <CalendarTile date={22} ></CalendarTile>
+                    <CalendarTile date={23} ></CalendarTile>
+                    <CalendarTile date={24} ></CalendarTile>
+                    <CalendarTile date={25} ></CalendarTile>
+                    <CalendarTile date={26} ></CalendarTile>
+                    <CalendarTile date={28} ></CalendarTile>
+                    <CalendarTile date={29} ></CalendarTile>
+                    <CalendarTile date={30} ></CalendarTile>
+                    <CalendarTile date={31}  ></CalendarTile>
                 </div>
             </div> 
         </div>
--- src/Components/CalendarView.js
+++ src/Components/CalendarView.js
@@ -2,14 +2,88 @@ import { useState, useContext, createContext } from "react";
 import Calendar from './Calendar.js';
 import BirthdayModal from './BirthdayModal.js';
 import Modal from '@mui/joy/Modal';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
+export const MONTHS = {
+    JANUARY: "January",
+    FEBRUARY : "February",
+    MARCH : "March",
+    APRIL : "April",
+    MAY : "May",
+    JUNE : "June",
+    JULY : "July",
+    AUGUST : "August",
+    SEPTEMBER : "September",
+    OCTOBER : "October",
+    NOVEMBER : "November", 
+    DECEMBER : "December"
+}
 
 export default function CalendarView () {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedMonth, setSelectedMonth] = useState(MONTHS.JANUARY);
+    let open = Boolean(anchorEl);
+
+    const handleMonthSelect = (event) => {
+        let month = event.currentTarget.innerText;
+        if( month==="April" || month==="June" || month==="September" || month==="November"){
+            //setThirtyOneDays(false);
+        }
+        setSelectedMonth(month);
+        setAnchorEl(null);
+    }
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget.innerHTML);
+    };
+  
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return(
         <div>
             <h3 class="title">Welcome to the Calendar Manager. Here, you can manage all of your birthdays
                 in a convenient calendar view.
             </h3>
-            <Calendar ></Calendar>
+            <div style={{float: 'left',marginLeft:"7%", display:'flex', alignItems:'center', width:'15%', columnGap:'5%'}}>
+                <h1>{selectedMonth}</h1>
+                <Menu anchorEl={anchorEl}
+                    open={open}
+                   >   
+                    <MenuItem selected={selectedMonth===MONTHS.JANUARY} onClick={(event)=>handleMonthSelect(event)}>January</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.FEBRUARY} onClick={(event)=>handleMonthSelect(event)}>February</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.MARCH} onClick={(event)=>handleMonthSelect(event)}>March</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.APRIL} onClick={(event)=>handleMonthSelect(event)}>April</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.MAY} onClick={(event)=>handleMonthSelect(event)}>May</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.JUNE} onClick={(event)=>handleMonthSelect(event)}>June</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.JULY} onClick={(event)=>handleMonthSelect(event)}>July</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.AUGUST} onClick={(event)=>handleMonthSelect(event)}>August</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.SEPTEMBER} onClick={(event)=>handleMonthSelect(event)}>September</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.OCTOBER} onClick={(event)=>handleMonthSelect(event)}>October</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.NOVEMBER} onClick={(event)=>handleMonthSelect(event)}>November</MenuItem>
+                    <MenuItem selected={selectedMonth===MONTHS.DECEMBER} onClick={(event)=>handleMonthSelect(event)}>December</MenuItem>
+                </Menu>
+                    <div style={{width:'50%'}}>
+                        <ArrowDropDownIcon style={{display:anchorEl ? 'none': 'block'}}onClick={handleMenuOpen}></ArrowDropDownIcon>
+                        <ArrowDropUpIcon style={{display:anchorEl ? 'block': 'none'}}onClick={()=>setAnchorEl(null)}></ArrowDropUpIcon>
+                    </div>
+                </div>
+            <div  style={{visible: selectedMonth===MONTHS.JANUARY ? 'visible': 'hidden'}}><Calendar month={MONTHS.JANUARY}></Calendar></div>
+            <Calendar style={{visible: selectedMonth===MONTHS.FEBRUARY ? 'visible': 'hidden'}} month={MONTHS.FEBRUARY}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.MARCH ? 'visible': 'hidden'}} month={MONTHS.MARCH}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.APRIL ? 'visible': 'hidden'}} month={MONTHS.APRIL}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.MAY ? 'visible': 'hidden'}}  month={MONTHS.MAY}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.JUNE ? 'visible': 'hidden'}} month={MONTHS.JUNE}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.JULY ? 'visible': 'hidden'}} month={MONTHS.JULY}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.AUGUST ? 'visible': 'hidden'}}  month={MONTHS.AUGUST}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.SEPTEMBER ? 'visible': 'hidden'}} month={MONTHS.SEPTEMBER}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.OCTOBER ? 'visible': 'hidden'}}  month={MONTHS.OCTOBER}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.NOVEMBER ? 'visible': 'hidden'}} month={MONTHS.NOVEMBER}></Calendar>
+            <Calendar style={{visible: selectedMonth===MONTHS.DECEMBER ? 'visible': 'hidden'}} month={MONTHS.DECEMBER}></Calendar>
         </div>
     );
 }
